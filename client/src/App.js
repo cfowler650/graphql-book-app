1c9346f136e85ad08732be8e5fa6707b627b027e
cfowler650 <cfowler650@gmail.com>
2019-10-31 09:22:01 -0500

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
+
 //components
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
 
 //apollo client setup
 const client = new ApolloClient({
@@ -16,6 +18,7 @@ function App() {
             <div id="main">
                 <h1>Apollo's Reading List</h1>
                 <BookList />
+                <AddBook />
             </div>
         </ApolloProvider>
     );
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
+import { getBooksQuery } from '../queries/queries';
 
-const getBooksQuery = gql`
-    {
-        books {
-            name
-            id
-        }
-    }
-`
+//components
+import BookDetails from './BookDetails';
+
 
 class BookList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: null
+        }
+    }
     displayBooks() {
         var data = this.props.data;
         if (data.loading) {
@@ -23,7 +24,7 @@ class BookList extends Component {
         } else {
             return data.books.map(book => {
                 return (
-                    <li key={book.id}>{book.name}</li>
+                    <li key={book.id} onClick={(e) => this.setState({ selected: book.id })}>{book.name}</li>
                 );
             })
         }
@@ -35,11 +36,10 @@ class BookList extends Component {
                 <ul id="book-list">
                     {this.displayBooks()}
                 </ul>
+                <BookDetails bookId={this.state.selected} />
             </div>
         );
     }
 }
-
-
 
 export default graphql(getBooksQuery)(BookList);
